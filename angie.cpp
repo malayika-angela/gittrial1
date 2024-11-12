3636a53388bc221abdf7b80ed7bb9d0f799b74a5
malayika-angela <malayikanawagi@gmail.com>
2024-11-12 14:31:15 +0300

    trial

---
+++ angie.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+double num1, num2; 
+char operation;
+
+int main(){
+    cout<<"Enter the first number"<<endl;
+    cin>>num1;
+    cout<<""
+
+    break;
+
+    case;
+    cout<<+,*,/<<endl;
+    break;
+
+    case;
+    cout<<enter the second number<<endl;
+    cin>> num1 *num 2;
+    break;
+    
+
+ default:
+ cout<<"not clear";
+    break;
+ }
+
